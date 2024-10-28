386a05995072ba018de5a130c99e02cb4a6b3fb6
PFlak <piotr.andrzej.flak@gmail.com>
2024-10-28 23:32:46 +0100

    setup run_docker.sh file and basic Dockerfile

---
+++ run_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+
+ROOT_DIR=$(pwd)
+VERSION=_v0
+argc=$#
+
+# Assign docker image name
+if [ $argc -eq 1 ] ; then
+    docker_name=$1
+    docker_name=${docker_name,,}
+else
+    docker_name=agh_marines_ros2$VERSION
+fi
+
+# Check if there is existing docker image
+search_image=$(docker images | grep -c $docker_name)
+
+if [ $search_image -eq 0 ]; then
+    # No image found, creating new one
+    echo "No image found, creating new one"
+    docker image build -t $docker_name $ROOT_DIR
+fi
+
+# starts an image
+echo "Starting image"
+docker run -it --rm -v $ROOT_DIR/src:/my_source_code $docker_name
